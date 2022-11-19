26d6097c5d57142f1bac1a7d9549dc6199f33ba0
Jesse Conner <jconner90@gmail.com>
2022-11-19 13:49:23 -0500

    Convert to a class to keep the passing stuff around down

--- src/helpers/transformers/inlineStyles.ts
+++ src/helpers/transformers/inlineStyles.ts
@@ -1,64 +1,77 @@
 import { parseCSS } from '../css';
-import { Declaration, Rule } from 'css';
+import { AtRule, Declaration, Rule, Stylesheet } from 'css';
 import { SVGSubElement } from '../../types';
 
-const cssPropToHtmlAttributeMap = {
-	color: 'stroke',
-	fill: 'fill',
-	'stroke-width': 'stroke-width',
-	'fill-opacity': 'stroke-opacity',
-	'letter-spacing': 'letter-spacing',
-	'font-family': 'font-family',
-	'font-size': 'font-size',
-	'font-style': 'font-style',
-	'font-variant': 'font-variant',
-	'font-weight': 'font-weight',
-	opacity: 'opacity',
-	rotate: 'rotate',
-} as const;
+class CSSInliner {
+	private readonly rules: Array<Rule | Comment | AtRule>;
+	public constructor(
+		private svgElem: SVGSVGElement,
+		private stylesheet: Stylesheet,
+		private options?: IInliningOptions
+	) {
+		this.rules = this.stylesheet?.stylesheet?.rules || [];
 
-function flattenSelector(rule: Rule) {
-	if (!rule.selectors) return '';
-	const combinedSelectors = rule.selectors.reduce(
-		(acc, selector) => acc + selector + ', ',
-		''
-	);
-	return combinedSelectors.slice(0, -2) || '';
-}
-
-function processElement(
-	svgElem: SVGSVGElement,
-	element: SVGSubElement,
-	rule: Rule,
-	options: IInliningOptions | undefined
-) {
-	if (!rule.declarations) return;
-	for (const declaration of rule.declarations as Declaration[]) {
-		if (
-			declaration.type !== 'declaration' ||
-			!declaration.property ||
-			!declaration.value
-		)
-			continue;
-		if (Object.hasOwn(cssPropToHtmlAttributeMap, declaration.property)) {
-			element.setAttribute(declaration.property, declaration.value);
+		for (const rule of this.rules) {
+			if ('type' in rule && rule.type === 'rule') {
+				this.processRule(rule as Rule);
+			}
 		}
 	}
-}
 
-function processRule(
-	svgElem: SVGSVGElement,
-	rule: Rule,
-	options: IInliningOptions | undefined
-) {
-	if (!rule.selectors) return;
-	const flattenedSelector = flattenSelector(rule);
-	if (!flattenedSelector) return;
-	const applicableElements = Array.from(
-		svgElem.querySelectorAll(flattenedSelector)
-	) as SVGSubElement[];
-	for (const element of applicableElements) {
-		processElement(svgElem, element, rule, options);
+	private cssPropToHtmlAttributeMap = {
+		color: 'stroke',
+		fill: 'fill',
+		'stroke-width': 'stroke-width',
+		'fill-opacity': 'stroke-opacity',
+		'letter-spacing': 'letter-spacing',
+		'font-family': 'font-family',
+		'font-size': 'font-size',
+		'font-style': 'font-style',
+		'font-variant': 'font-variant',
+		'font-weight': 'font-weight',
+		opacity: 'opacity',
+		rotate: 'rotate',
+	} as const;
+
+	private flattenSelector(rule: Rule) {
+		if (!rule.selectors) return '';
+		const combinedSelectors = rule.selectors.reduce(
+			(acc, selector) => acc + selector + ', ',
+			''
+		);
+		return combinedSelectors.slice(0, -2) || '';
+	}
+
+	private processElement(element: SVGSubElement, rule: Rule) {
+		if (!rule.declarations) return;
+		for (const declaration of rule.declarations as Declaration[]) {
+			if (
+				declaration.type !== 'declaration' ||
+				!declaration.property ||
+				!declaration.value
+			)
+				continue;
+			if (
+				Object.hasOwn(
+					this.cssPropToHtmlAttributeMap,
+					declaration.property
+				)
+			) {
+				element.setAttribute(declaration.property, declaration.value);
+			}
+		}
+	}
+
+	private processRule(rule: Rule) {
+		if (!rule.selectors) return;
+		const flattenedSelector = this.flattenSelector(rule);
+		if (!flattenedSelector) return;
+		const applicableElements = Array.from(
+			this.svgElem.querySelectorAll(flattenedSelector)
+		) as SVGSubElement[];
+		for (const element of applicableElements) {
+			this.processElement(element, rule);
+		}
 	}
 }
 
@@ -67,13 +80,8 @@ export function inlineStyles(
 	options?: IInliningOptions
 ): void {
 	const styles = parseCSS(svgElem.querySelector('style')?.textContent || '');
-	const rules = styles?.stylesheet?.rules;
-	if (!rules) return;
-	for (const rule of rules) {
-		if (rule.type === 'rule') {
-			processRule(svgElem, rule, options);
-		}
-	}
+	if (!styles) return;
+	new CSSInliner(svgElem, styles, options);
 }
 
 export interface IInliningOptions {}
