0e60d1c98675e9a7862742bbca296a5311115f45
Jesse Conner <jconner90@gmail.com>
2022-11-19 17:34:53 -0500

    Cleanup attribute stripping closes #8

---
+++ src/helpers/transformers/stripAttribute.ts
@@ -0,0 +1,8 @@
+import { traverseTree } from 'helpers/dom';
+
+export function stripAttribute(svgElem: Element, attribute: string) {
+	const func = (elem: Element) => {
+		elem.removeAttribute(attribute);
+	};
+	traverseTree(svgElem, func);
+}
