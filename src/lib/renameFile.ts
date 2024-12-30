d41e70d6db8aac4415404807d843767d8a102e64
Jesse Conner <me@jesseconner.ca>
2024-12-29 19:37:30 -0500

    Renaming internals for #27

---
+++ src/lib/renameFile.ts
@@ -0,0 +1,15 @@
+import type { FileProps } from "@/lib/types.js";
+
+/**
+ * Renames a file
+ *
+ * @param newName The new name of the file
+ * @param fileProps The file properties
+ */
+export function renameFile(newName: string, fileProps:FileProps):void {
+	const { stateTuple } = fileProps;
+	const [state, setState] = stateTuple;
+	const newState = { ...state };
+	newState.file.title = newName;
+	setState(newState);
+}
---
+++ tests/lib/renameFile.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect, vi } from "vitest";
+
+import { renameFile } from "@/lib/renameFile.js";
+
+describe("renameFile", () => {
+	it("should rename a file", () => {
+		const file = {
+			file: { title: "test-1", contents: "test-1" },
+			previous: [],
+		};
+		const setState = vi.fn();
+		renameFile("test-2", { stateTuple: [file, setState] });
+		expect(setState).toHaveBeenCalledWith({
+			file: { title: "test-2", contents: "test-1" },
+			previous: [],
+		});
+		expect(file.file.title).toBe("test-1");
+	});
+});
