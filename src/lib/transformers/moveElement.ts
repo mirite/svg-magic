6305601f7b505c12d595fd804c4d09745b1378bb
Jesse Conner <me@jesseconner.ca>
2024-12-30 14:56:31 -0500

    Switch element reordering to ids

--- src/lib/transformers/moveElement.ts
+++ src/lib/transformers/moveElement.ts
@@ -1,26 +1,37 @@
-import { findShadowEquivalent } from "../dom.js";
+import { traverseTree } from "../dom.js";
 
+import { getSVGChildren } from "@/lib/getSVGChildren.js";
 import type { SVGSubElement } from "@/lib/types.js";
 
 export interface IMoveOptions {
-	element: SVGSubElement;
-	target: SVGSubElement;
+	element: number;
+	target: number;
 }
 
 /**
  * Moves an element in the tree.
  *
- * @param shadowContainer A reference to the SVG for manipulation.
+ * @param svgElement A reference to the SVG for manipulation.
  * @param change The change options.
  */
 export function moveElement(
-	shadowContainer: SVGElement,
+	svgElement: SVGElement,
 	change: IMoveOptions,
 ): void {
-	const { target, element } = change;
-	const targetEquiv = findShadowEquivalent(target, shadowContainer);
-	const elementEquiv = findShadowEquivalent(element, shadowContainer);
+	const rootNode = getSVGChildren(svgElement);
+	let targetEquiv: SVGSubElement | null = null;
+	let elementEquiv: SVGSubElement | null = null;
+
+	traverseTree(rootNode, ({ elem, id }) => {
+		if (change.target === id) {
+			targetEquiv = elem;
+		} else if (change.element === id) {
+			elementEquiv = elem;
+		}
+	});
+
 	if (targetEquiv && elementEquiv) {
+		// @ts-expect-error TS2339 TypeScript doesn't recognize that these can be set in the traverseTree callback.
 		targetEquiv.append(elementEquiv);
 	}
 }
---
+++ tests/lib/transformers/moveElement.test.ts
@@ -0,0 +1,31 @@
+import { describe, it, expect } from "vitest";
+
+import { createSVG } from "../../helpers.js";
+
+import { moveElement } from "@/lib/transformers/index.js";
+import type { SVGSubElement } from "@/lib/types.js";
+
+describe("moveElement", () => {
+	it("should move an element to a new parent", () => {
+		const svg = createSVG(`
+			<svg>
+				<g id="parent">
+					<rect id="child" />
+				</g>
+				<g id="newParent" />
+			</svg>
+		`);
+
+		const parent = svg.querySelector("#parent") as SVGSubElement;
+		const child = svg.querySelector("#child") as SVGSubElement;
+		const newParent = svg.querySelector("#newParent") as SVGSubElement;
+
+		moveElement(svg, {
+			element: 1001,
+			target: 2,
+		});
+
+		expect(parent.contains(child)).toBe(false);
+		expect(newParent.contains(child)).toBe(true);
+	});
+});
