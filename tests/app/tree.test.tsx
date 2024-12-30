d2274cc0666b17b2a69a90649a34e026f56ddd80
Jesse Conner <me@jesseconner.ca>
2024-12-29 19:26:12 -0500

    Tests

--- tests/app/tree.test.tsx
+++ tests/app/tree.test.tsx
@@ -87,18 +87,8 @@ describe("<Tree>", () => {
 			<Tester testSVG={`<svg><g><line></line></g></svg>`} Component={Tree} />,
 		);
 		expect(getByTestId("tester")).toBeTruthy();
-		const group = getByTestId("node-1");
-		expect(group).toBeTruthy();
-		const line = getByTestId("node-1001");
-		expect(line).toBeTruthy();
-		const lineCheckbox = line.querySelector("input");
-		await act(async () => {
-			lineCheckbox?.click();
-		});
-		const groupCheckbox = group.querySelector("input");
-		await act(async () => {
-			groupCheckbox?.click();
-		});
+		await checkNode(getByTestId, 1001);
+		await checkNode(getByTestId, 1);
 		const assignClassForm = await assignNewClass(queryByTestId);
 		const submitButton = assignClassForm?.querySelector("button");
 		await act(async () => {
@@ -110,6 +100,55 @@ describe("<Tree>", () => {
 		const group2 = getByTestId("node-1");
 		expect(group2).toBeTruthy();
 		expect(group2.textContent).toContain("test");
+	});
+
+	it("Should allow for selected elements to be grouped", async () => {
+		const { getByTestId } = render(
+			<Tester
+				testSVG={`<svg><line></line><line></line></svg>`}
+				Component={Tree}
+			/>,
+		);
+		expect(getByTestId("tester")).toBeTruthy();
+		await checkNode(getByTestId, 1);
+		await checkNode(getByTestId, 2);
+		const groupForm = getByTestId("add-group");
+		expect(groupForm).toBeTruthy();
+		const groupInput = groupForm.querySelector("input") as HTMLInputElement;
+		expect(groupInput).toBeTruthy();
+		await act(async () => {
+			fireEvent.input(groupInput, {
+				target: {
+					value: "test",
+				},
+			});
+		});
+		expect(groupInput.value).toEqual("test");
+		await act(async () => {
+			fireEvent.submit(groupForm);
+		});
+		const group = getByTestId("node-1");
+		expect(group).toBeTruthy();
+		expect(group.textContent).toContain("test");
+		const groupChildA = getByTestId("node-1001");
+		expect(groupChildA).toBeTruthy();
+		const groupChildB = getByTestId("node-1002");
+		expect(groupChildB).toBeTruthy();
+	});
+
+	it("Should allow an empty group to be created", async () => {
+		const { getByTestId } = render(
+			<Tester testSVG={`<svg></svg>`} Component={Tree} />,
+		);
+		expect(getByTestId("tester")).toBeTruthy();
+		const groupForm = getByTestId("add-group");
+		expect(groupForm).toBeTruthy();
+		await act(async () => {
+			fireEvent.submit(groupForm);
+		});
+		const group = getByTestId("node-1");
+		expect(group).toBeTruthy();
+		expect(group.querySelector("ul")).toEqual(null);
 	});
 });
 
@@ -149,4 +188,22 @@ async function assignNewClass(
 	});
 	expect(textInput.value).toEqual(className);
 	return assignClassForm as HTMLFormElement;
+}
+
+/**
+ * Toggle the checkbox for a node.
+ *
+ * @param getByTestId The getByTestId function.
+ * @param nodeID The ID of the node to toggle.
+ */
+async function checkNode(
+	getByTestId: (id: string) => HTMLElement,
+	nodeID: number,
+) {
+	const node = getByTestId(`node-${nodeID}`);
+	const input = node.querySelector("input");
+	expect(input).toBeTruthy();
+	await act(async () => {
+		input?.click();
+	});
 }
