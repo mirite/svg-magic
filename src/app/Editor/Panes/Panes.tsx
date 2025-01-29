50042c36323a5b3ec202c821486f3ef84331bd4e
Jesse Conner <me@jesseconner.ca>
2025-01-29 18:35:48 -0500

    Cleaning up imports

--- src/app/Editor/Panes/Panes.tsx
+++ src/app/Editor/Panes/Panes.tsx
@@ -21,7 +21,7 @@ function Panes(props: IProps): ReactElement {
 	return (
 		<div
 			className={
-				"grid grid-cols-2 inset-x-0 xl:grid-cols-4 h-[calc(100dvh_-_var(--content-top))] top-[--content-top] fixed overflow-hidden"
+				"grid grid-cols-2 inset-x-0 xl:grid-cols-4 h-[calc(100dvh_-_var(--content-top))] top-(--content-top) fixed overflow-hidden"
 			}
 		>
 			{panes.map((Pane, index) => (
--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -9,7 +9,7 @@ export const Header: ComponentType<{
 	goHome: () => void;
 }> = (props) => {
 	return (
-		<header className="flex gap-2 items-center m-2 mb-0 fixed top-0 inset-x-0 h-[--nav-height]">
+		<header className="flex gap-2 items-center m-2 mb-0 fixed top-0 inset-x-0 h-(--nav-height)">
 			<button
 				className="cursor-pointer text-2xl mb-2"
 				onClick={() => props.goHome()}
